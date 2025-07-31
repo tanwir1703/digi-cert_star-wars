e26c0fef9afeec536655c1429aa9af2fe2907ccd
tanwir1703 <tanwir1703@gmail.com>
2025-07-31 23:38:28 +0530

    Deployment fix

--- star-wars-ui/vite.config.js
+++ star-wars-ui/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/digi-cert_star-wars/star-wars-ui/',
+  base: '/digi-cert_star-wars/',
   plugins: [react()],
 });
+
